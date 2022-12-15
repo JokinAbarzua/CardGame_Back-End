29762e971cf11d240f25edeccccecfc964214ffd
JokinAbarzua <jokinabarzua22@hotmail.com>
2022-12-15 18:52:53 -0300

    authentication

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,15 @@
+class AuthenticationController < ApplicationController   
+    include JsonWebToken
+    skip_before_action :authenticate, only: [:login]
+
+    #POST /auth/login
+    def login
+        @user = User.find_by(username: params[:username])
+        if @user&.authenticate(params[:password]) #el metodo authenticate es de bcrypt
+            token = JsonWebToken.jwt_encode(user_id: @user.id)
+            render json:{status: 200, token:token}
+        else
+            render json:{status: 401, message: "Usuario o Contraseña Invalidos"} 
+        end
+    end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,50 @@
+class UsersController < ApplicationController
+    before_action :set_user, only: [:show,:destroy, :update]
+    #GET /users
+    def index        
+        @users = User.all
+        render json:{status: 200, users: @users}
+    end
+
+    #GET    /users/:id
+    def show        
+        render json:{status: 200, user: @user}
+    end
+    
+    #POST   /users
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            render json:{status: 200, user: @user}
+        else
+            render json:{status: 400, message: @user.error.details}
+        end
+        
+    end
+    
+    #PUT    /users/:id  
+    def update
+        
+    end
+    
+    #DELETE /users/:id  
+    def destroy
+        if(@user.destroy)
+            render json:{status: 200, message: "Usuario Eliminado con Exito"}
+        else
+            render json:{status: 400, message: @user.error.details}
+        end
+    end
+    
+    
+
+    private
+
+        def set_user 
+            @user = User.find(params[:id])        
+        end
+
+        def user_params 
+            params.require(:user).permit(:username,:password)
+        end
+end
